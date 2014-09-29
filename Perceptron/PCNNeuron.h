e0354f63a85e1dcddadf1bd90e3ddec687aa4f64
rutten1994 <lourfinus@yahoo.com>
2014-09-30 00:27:55 +0300

    added model classes' files

---
+++ Perceptron/PCNNeuron.h
@@ -0,0 +1,13 @@
+//
+//  PCNNeuron.h
+//  Perceptron
+//
+//  Created by Artyom Gavryushov on 30.09.14.
+//  Copyright (c) 2014 Gavr corp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PCNNeuron : NSObject
+
+@end
---
+++ Perceptron/PCNNeuron.m
@@ -0,0 +1,13 @@
+//
+//  PCNNeuron.m
+//  Perceptron
+//
+//  Created by Artyom Gavryushov on 30.09.14.
+//  Copyright (c) 2014 Gavr corp. All rights reserved.
+//
+
+#import "PCNNeuron.h"
+
+@implementation PCNNeuron
+
+@end
