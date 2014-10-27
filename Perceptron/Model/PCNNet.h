576f81255e7a01cc7ca94b3a233a422d7c2f6931
Artyom Gavryushov <artyomgavryushov@MacBook-Pro-Artyom.local>
2014-10-28 00:55:03 +0200

    created PCNNet class

---
+++ Perceptron/Model/PCNNet.h
@@ -0,0 +1,13 @@
+//
+//  PCNNet.h
+//  Perceptron
+//
+//  Created by Artyom Gavryushov on 28.10.14.
+//  Copyright (c) 2014 Gavr corp. All rights reserved.
+//
+
+#import "PCNLayer.h"
+
+@interface PCNNet : PCNLayer
+
+@end
---
+++ Perceptron/Model/PCNNet.m
@@ -0,0 +1,13 @@
+//
+//  PCNNet.m
+//  Perceptron
+//
+//  Created by Artyom Gavryushov on 28.10.14.
+//  Copyright (c) 2014 Gavr corp. All rights reserved.
+//
+
+#import "PCNNet.h"
+
+@implementation PCNNet
+
+@end
